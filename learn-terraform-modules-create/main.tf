23962562fb1b470efe4508b5882a36312f2fa166
Oleksii Honchar <tuiteraz@gmail.com>
2023-08-09 19:12:43 +0200

    feat(aws-s3-static-website-bucket): added static files and cp, rm to s3 via make

--- learn-terraform-modules-create/main.tf
+++ learn-terraform-modules-create/main.tf
@@ -9,39 +9,36 @@ terraform {
   }
 }
 
-#module "vpc" {
-#  source  = "terraform-aws-modules/vpc/aws"
-#  version = "5.1.1"
-#
-#  name = var.vpc_name
-#  cidr = var.vpc_cidr
-#
-#  azs             = var.vpc_azs
-#  private_subnets = var.vpc_private_subnets
-#  public_subnets  = var.vpc_public_subnets
-#
-#  enable_nat_gateway = var.vpc_enable_nat_gateway
-#
-#  tags = var.vpc_tags
-#}
-#
-#module "ec2_instances" {
-#  source  = "terraform-aws-modules/ec2-instance/aws"
-#  version = "5.2.1"
-#
-#  count = 1
-#  name  = "my-ec2-cluster-${count.index}"
-#
-#  ami                    = "ami-09af72ad2a3676ba8"
-#  instance_type          = "t2.micro"
-#  vpc_security_group_ids = [module.vpc.default_security_group_id]
-#  subnet_id              = module.vpc.public_subnets[0]
-#
-#  tags = {
-#    Terraform   = "true"
-#    Environment = "dev"
-#  }
-#}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.2.1"
+
+  count = 1
+  name  = "my-ec2-cluster-${count.index}"
+
+  ami                    = "ami-09af72ad2a3676ba8"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = var.tags
+}
 
 
 module "website_s3_bucket" {
