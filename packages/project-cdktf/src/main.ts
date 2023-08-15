61be597027acfe160ce7cf9267adc72e7380debf
Oleksii Honchar <tuiteraz@gmail.com>
2023-08-15 20:55:53 +0200

    feat(cdktf): s3 website wip

--- packages/project-cdktf/src/main.ts
+++ packages/project-cdktf/src/main.ts
@@ -1,7 +1,9 @@
 import { App } from "cdktf";
 
 import { GlobalS3StateStack } from "./cdktf-stacks/GlobalS3StateStack.ts";
+import { StaticS3WebsiteStack } from "./cdktf-stacks/StaticS3Website.ts";
 
 const app = new App();
 new GlobalS3StateStack(app, "GlobalS3State");
+new StaticS3WebsiteStack(app, "StaticS3Website");
 app.synth();
